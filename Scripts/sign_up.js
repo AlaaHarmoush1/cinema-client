9a5a6fcddcb5d12e8f7e61ec932e760840a78c63
AlaaHarmoush1 <Alaacoding@outlook.com>
2025-06-25 13:37:51 +0300

    Created sign up logic to handle user creation in the database

--- Scripts/sign_up.js
+++ Scripts/sign_up.js
@@ -1,1 +1,47 @@
-console.log("Hello, World!");
+document.getElementById("Signup").addEventListener('click', async (event)=> {
+    event.preventDefault();
+    
+    let name = document.getElementById('name').value;
+    let email_phone = document.getElementById('email-phone').value;
+    let password = document.getElementById('password').value;
+
+    console.log(name)
+    console.log(email_phone)
+    console.log(password)
+
+    let phone = null;
+    let email = null;
+
+    if(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email_phone)){
+        email = email_phone;
+        phone = null;
+
+        console.log(`Email is ${email}  && your mobile is ${phone}`)
+    }else if(/^\d+$/.test(email_phone) && email_phone.length == 8){
+        phone = email_phone;
+        email = null;
+        console.log(`Email is ${email}  && your mobile is ${phone}`)
+    }else{
+        alert('Invalid Email or Phone Number')
+    }
+
+    try {
+        let response = await axios.post('http://localhost/Cinema-server/controllers/auth_controller.php', {
+            name: name,
+            email: email,
+            password: password,
+            phone_number: phone   
+        },
+        {
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }
+    )
+    }catch(error){
+        alert("The error is " + error)
+    }
+
+
+})
+
